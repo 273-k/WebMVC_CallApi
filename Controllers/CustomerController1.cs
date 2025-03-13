3311a13b784f3033dd9f9badbb388df0ca07247d
273-k <alvin.abdullayev@gmail.com>
2025-03-13 11:06:53 +0400

    Add project files.

---
+++ Controllers/CustomerController1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebMVC_CallApi.Models;
+
+namespace WebMVC_CallApi.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private const string ApiBaseUrl = "http://localhost:5066/api/customers";
+
+        public CustomerController()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var customers = await _httpClient.GetFromJsonAsync<List<Customer>>(ApiBaseUrl);
+            return View(customers);
+        }
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Customer customer)
+        {
+            var response = await _httpClient.PostAsJsonAsync(ApiBaseUrl, customer);
+            if (response.IsSuccessStatusCode)
+                return RedirectToAction("Index");
+
+            return View(customer);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var customer = await _httpClient.GetFromJsonAsync<Customer>($"{ApiBaseUrl}/{id}");
+            return View(customer);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Customer customer)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"{ApiBaseUrl}/{customer.Id}", customer);
+            if (response.IsSuccessStatusCode)
+                return RedirectToAction("Index");
+
+            return View(customer);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"{ApiBaseUrl}/{id}");
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace WebMVC_CallApi.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public int RegionId { get; set; }
+    }
+}
